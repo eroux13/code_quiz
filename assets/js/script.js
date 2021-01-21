36fbf77b1da1a38586a11a9219dc44ecf5129ab9
EJ <ej.roux13@gmail.com>
2021-01-21 15:15:11 -0800

    added functionality to display user choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,22 @@ var answerDisplay = document.querySelector("#answerDisplay");
 var startBtn = document.querySelector("#startBtn");
 
 // Global Variables
-var timerStart = 5;
+var timerStart = 5; // reset to 76 when complete
+var questionIndex = 0;
+
+// Questions & Answers Object
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "Front-end development includes: HTML, CSS, and ________",
+        choices: ["JavaScript", "C++", "Java", "Swift"],
+        answer: "JavaScript"
+    }
+]
 
 // Timer Function
 function setTime() {
@@ -32,6 +47,23 @@ startBtn.addEventListener("click", function () {
     setTime();
     startBtn.setAttribute("class", "hidden");
     pTag.setAttribute("class", "hidden");
-    // Display Question & Choices
+    generateQuestion();
 })
 
+// Display Question & Choices
+function generateQuestion() {
+    for (var i = 0; i < questions.length; i++) {
+        var quizQuestion = questions[questionIndex].title;
+        var userChoices = questions[questionIndex].choices;
+        h1Element.textContent = quizQuestion;
+    }
+    userChoices.forEach(function (newItem) {
+        var userAnswerBtn = document.createElement("button");
+        var br = document.createElement("br");
+        userAnswerBtn.setAttribute("class", "btn btn-primary answerBtn");
+        userAnswerBtn.textContent = newItem;
+        answerDisplay.append(userAnswerBtn);
+        answerDisplay.append(br);
+        // Create function to compare answers if correct/incorrect
+    })
+}
